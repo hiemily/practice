e44ffe4de70b4a4629de2b91b6ce6822273d40f0
김현정 <emily00@teamblind.com>
2017-09-21 19:58:15 +0900

    searchbar 추가, 검색기능 추가

--- KoreaMountains/ViewController.swift
+++ KoreaMountains/ViewController.swift
@@ -17,22 +17,60 @@ struct mountainInfo {
     var mDetail:String? //산정보 상세
 }
 
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+/**
+ * TODO LIST
+ - searchbar 추가
+ - refreshcontroller 추가 (runtime error 해결)
+ - indicator 추가
+ - didselect -> 화면 푸시, 산 정보 상세 조회
+ */
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {//}, UISearchResultsUpdating {
 
     @IBOutlet weak var tableview: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
     var mList = [mountainInfo]()
-    var refreshControler : UIRefreshControl!
+    var refreshControl : UIRefreshControl!
+    var isSearching : Bool!
+    let searchController = UISearchController(searchResultsController: nil)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
-        tableview.refreshControl = refreshControler
+
+        searchBar.placeholder = "알고 싶은 산 이름을 입력하세요."
+        tableview.tableHeaderView = searchBar
+        // set up searchController
+//        searchController.searchResultsUpdater = self
+//        searchController.dimsBackgroundDuringPresentation = true
+//        tableview.tableHeaderView = searchController.searchBar
+        
+        // set up refreshController
+//        refreshControl = UIRefreshControl()
+//        tableview.refreshControl = refreshControl
+//        
+//        refreshControl.addTarget(self, action: #selector(refreshData), for: .valueChanged)
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        getMountainInfo()
+        getMountainInfo(nil)
+    }
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        let keyword = searchController.searchBar.text!
+        mList.removeAll()
+        isSearching = true
+        getMountainInfo(keyword)
+    }
+
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+        
+        let keyword = searchBar.text!
+        mList.removeAll()
+        isSearching = true
+        getMountainInfo(keyword)
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -48,9 +86,22 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         return cell
     }
     
-    func getMountainInfo() {
+    func refreshData() {
+        //--------------- runtime error ----------------
+        // data 초기화
+        mList.removeAll()
+        isSearching = true
+        
+        getMountainInfo(nil)
+        refreshControl.endRefreshing()
+    }
+    
+    func getMountainInfo(_ searchKeyword : String?) {
         var parameters = [String : String]()
-//        parameters["searchWrd"] = "북한산"
+        if let searchKeyword = searchKeyword
+        {
+            parameters["searchWrd"] = searchKeyword
+        }
         parameters["pageNo"] = String(1)
         parameters["numOfRows"] = String(20)
         
@@ -61,31 +112,45 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
             if let items = json["item"] as? [[String:Any]] {
                 for item in items {
                     var mountain = mountainInfo()
-                    
-                    if let name = item["mntiname"] as? String {
-                        mountain.mName = name
-                        print("name : \(name)")
-                    }
-                    
-                    if let id = item["mntilistno"] as? Int {
-                        mountain.mId = id
-                        print("id : \(id)")
-                    }
-                    
-                    if let addr = item["mntiadd"] as? String {
-                        mountain.mAddr = addr
-                        print("addr : \(addr)")
-                    }
-                    
-                    if let detail = item["mntidetails"] as? String {
-                        mountain.mDetail = detail
-                    }
-                    
+                    mountain = self.makeMInfoObject(obj: item)
                     self.mList.append(mountain)
                 }
+                self.isSearching = false
+                self.tableview.reloadData()
+            } else if let item = json["item"] as? [String:Any] {
+                var mountain = mountainInfo()
+                mountain = self.makeMInfoObject(obj: item)
+                self.mList.append(mountain)
+                
+                self.isSearching = false
                 self.tableview.reloadData()
             }
         })
+    }
+    
+    func makeMInfoObject(obj:[String:Any]) -> mountainInfo {
+        var mountain = mountainInfo()
+        
+        if let name = obj["mntiname"] as? String {
+            mountain.mName = name
+            print("name : \(name)")
+        }
+        
+        if let id = obj["mntilistno"] as? Int {
+            mountain.mId = id
+            print("id : \(id)")
+        }
+        
+        if let addr = obj["mntiadd"] as? String {
+            mountain.mAddr = addr
+            print("addr : \(addr)")
+        }
+        
+        if let detail = obj["mntidetails"] as? String {
+            mountain.mDetail = detail
+        }
+        
+        return mountain
     }
     
     override func didReceiveMemoryWarning() {
